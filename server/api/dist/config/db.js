a7276557ebb0dc08a10ae4902f95f641dcb60d30
yaxx <shehuzaks@yahoo.com>
2023-03-15 13:08:18 +0100

    first build

--- server/api/dist/config/db.js
+++ server/api/dist/config/db.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.MONGO_URI = exports.MONGO_OPTIONS = void 0;
+const app_1 = require("./app");
 const { MONGO_HOST = 'localhost', MONGO_DATABASE = 'systo', MONGO_PORT = '27017', MONGO_USERNAME = '', MONGO_PASSWORD = '' } = process.env;
 exports.MONGO_OPTIONS = {
     useNewUrlParser: true,
@@ -8,5 +9,5 @@ exports.MONGO_OPTIONS = {
 };
 const LOCAL_URI = `mongodb+srv://me:Sqb0NzshutN74DaR@nextoma.2mkb8.mongodb.net/prod`;
 const REMOTE_URI = `mongodb+srv://${MONGO_USERNAME}:${encodeURIComponent(MONGO_PASSWORD)}@${MONGO_HOST}/${MONGO_DATABASE}`;
-exports.MONGO_URI = LOCAL_URI;
+exports.MONGO_URI = app_1.IN_PRODUCTION ? REMOTE_URI : LOCAL_URI;
 //# sourceMappingURL=db.js.map
