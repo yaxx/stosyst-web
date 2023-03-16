bfa4601e031cd9108af35c80796e493d089fa7b5
yaxx <shehuzaks@yahoo.com>
2023-03-16 11:31:44 +0100

    change port to 4000

--- server/api/src/config/app.ts
+++ server/api/src/config/app.ts
@@ -3,5 +3,4 @@ export const {
     SERVER_PORT = 4000,
 }: any = process.env
 
-export const IN_PRODUCTION = process.env.NODE_ENV === 'production' 
-  
+export const IN_PRODUCTION = process.env.NODE_ENV === 'production'
